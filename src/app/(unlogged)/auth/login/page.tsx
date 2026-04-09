b73a3d5f19170c6bc12a71d89f829c1016170cff
Daniel Durán <daninsoi924@gmail.com>
2026-04-09 01:37:16 -0400

    Update page.tsx

--- src/app/(unlogged)/auth/login/page.tsx
+++ src/app/(unlogged)/auth/login/page.tsx
@@ -8,7 +8,19 @@ import logoIcon from '../../../../../public/logo.png';
 
 export default function AuthPage() {
   const handleOAuthLogin = () => {
-    const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "";
+    // 1. Intentamos obtener la variable
+    let backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+    // 2. Si la variable falla pero estamos en Railway, la forzamos
+    if (!backendUrl && typeof window !== 'undefined') {
+      if (window.location.hostname.includes('railway.app')) {
+        backendUrl = 'https://projeicbackend-production.up.railway.app';
+      } else {
+        backendUrl = 'http://localhost:4000'; // Tu puerto local de NestJS
+      }
+    }
+
+    console.log("Redirigiendo a:", `${backendUrl}/projeic/api/auth/google`);
     window.location.href = `${backendUrl}/projeic/api/auth/google`;
   };
 
